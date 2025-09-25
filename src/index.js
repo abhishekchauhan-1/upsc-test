610fe51d53eddb4cf8681abdb03a9418de44e2eb
abhishekchauhan-1 <singhchauhanabhishek816@gmail.com>
2025-09-25 14:47:07 +0530

    new files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import './index.css'
 import App from "./App";  // आपका main App component
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
